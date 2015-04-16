38ebd17de47a3b28c3cb82f9dad1092d64a9c5d7
Andrei Straut <andrei.straut@gmail.com>
2015-04-16 10:01:34 +0100

    Translator unit tests

---
+++ src/test/java/DatamodelTranslator.java
@@ -0,0 +1,12 @@
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    com.andreistraut.gaps.datamodel.translator.GraphGeneticsTranslatorTest.class
+    })
+
+public class DatamodelTranslator {
+}
--- src/test/java/com/andreistraut/gaps/datamodel/translator/GraphGeneticsTranslatorTest.java
+++ src/test/java/com/andreistraut/gaps/datamodel/translator/GraphGeneticsTranslatorTest.java
@@ -4,45 +4,87 @@ import com.andreistraut.gaps.datamodel.genetics.EdgeGene;
 import com.andreistraut.gaps.datamodel.genetics.GeneticConfiguration;
 import com.andreistraut.gaps.datamodel.genetics.PathChromosome;
 import com.andreistraut.gaps.datamodel.graph.DirectedWeightedEdge;
+import com.andreistraut.gaps.datamodel.graph.DirectedWeightedGraphPath;
 import com.andreistraut.gaps.datamodel.graph.Node;
+import com.andreistraut.gaps.datamodel.mock.DirectedWeightedGraphMockThreeNodeThreeEdges;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.jgrapht.GraphPath;
-import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.BeforeClass;
 
 public class GraphGeneticsTranslatorTest {
-    
+
+    private DirectedWeightedGraphMockThreeNodeThreeEdges graphMock;
+    private GeneticConfiguration conf;
+    private GraphPath<Node, DirectedWeightedEdge> path;
+
     public GraphGeneticsTranslatorTest() {
     }
-    
+
+    @BeforeClass
+    public static void setUpClass() {
+	Logger.getLogger(GraphGeneticsTranslatorTest.class.getName()).log(Level.INFO,
+		"{0} TEST: GraphGeneticsTranslatorTest",
+		GraphGeneticsTranslatorTest.class.toString());
+    }
+
     @Before
     public void setUp() {
+	this.graphMock = new DirectedWeightedGraphMockThreeNodeThreeEdges();
+	this.conf = new GeneticConfiguration("PathChromosomePopulationTest", this.graphMock.getGraph());
+	this.path = graphMock.getGraph().getKShortestPaths(graphMock.getFirstNode(), graphMock.getThirdNode(), 2).get(1);
     }
 
     @Test
-    public void testToPathChromosome() throws Exception {
-	System.out.println("toPathChromosome");
-	GraphPath<Node, DirectedWeightedEdge> path = null;
-	GeneticConfiguration configuration = null;
-	PathChromosome expResult = null;
-	PathChromosome result = GraphGeneticsTranslator.toPathChromosome(path, configuration);
-	assertEquals(expResult, result);
-	// TODO review the generated test code and remove the default call to fail.
-	fail("The test case is a prototype.");
+    public void testToPathChromosomeValidPath() throws Exception {
+	PathChromosome chromosome = GraphGeneticsTranslator.toPathChromosome(path, conf);
+
+	Assert.assertTrue(chromosome.getGenesList().size() == 2);
+	Assert.assertTrue(chromosome.getGene(0).getAllele().equals(graphMock.getFirstToSecondEdge()));
+	Assert.assertTrue(chromosome.getGene(1).getAllele().equals(graphMock.getSecondToThirdEdge()));
     }
 
     @Test
-    public void testToEdgeGeneList() throws Exception {
-	System.out.println("toEdgeGeneList");
-	GraphPath<Node, DirectedWeightedEdge> edgeList = null;
-	GeneticConfiguration configuration = null;
-	ArrayList<EdgeGene> expResult = null;
-	ArrayList<EdgeGene> result = GraphGeneticsTranslator.toEdgeGeneList(edgeList, configuration);
-	assertEquals(expResult, result);
-	// TODO review the generated test code and remove the default call to fail.
-	fail("The test case is a prototype.");
+    public void testToPathChromosomeEmptyPath() throws Exception {
+	GraphPath<Node, DirectedWeightedEdge> invalidPath
+		= new DirectedWeightedGraphPath(graphMock.getGraph());
+
+	//Negative test, should fail for empty paths
+	try {
+	    PathChromosome chromosome = GraphGeneticsTranslator.toPathChromosome(invalidPath, conf);
+	    Assert.assertTrue(false);
+	} catch (NullPointerException e) {
+	    Assert.assertTrue(true);
+	}
     }
-    
+
+    @Test
+    public void testToEdgeGeneListValidPath() throws Exception {
+	ArrayList<EdgeGene> expected = new ArrayList<>();
+	expected.add(new EdgeGene(graphMock.getFirstToSecondEdge(), conf));
+	expected.add(new EdgeGene(graphMock.getSecondToThirdEdge(), conf));
+
+	ArrayList<EdgeGene> actual = GraphGeneticsTranslator.toEdgeGeneList(path, conf);
+	Assert.assertTrue(expected.equals(actual));
+    }
+
+    @Test
+    public void testToEdgeGeneListEmptyPath() throws Exception {
+
+	GraphPath<Node, DirectedWeightedEdge> invalidPath
+		= new DirectedWeightedGraphPath(graphMock.getGraph());
+
+	//Negative test, should fail for empty paths
+	try {
+	    ArrayList<EdgeGene> actual = GraphGeneticsTranslator.toEdgeGeneList(invalidPath, conf);
+	    Assert.assertTrue(false);
+	} catch (NullPointerException e) {
+	    Assert.assertTrue(true);
+	}
+    }
+
 }
