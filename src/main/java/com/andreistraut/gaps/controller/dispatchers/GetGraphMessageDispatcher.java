7de4b0ff3aeb1a87556ccca10ebe09491fbb11df
Andrei Straut <andrei.straut@gmail.com>
2015-04-21 23:03:19 +0100

    Refactor class property, expose getter

--- src/main/java/com/andreistraut/gaps/controller/dispatchers/GetGraphMessageDispatcher.java
+++ src/main/java/com/andreistraut/gaps/controller/dispatchers/GetGraphMessageDispatcher.java
@@ -13,68 +13,73 @@ import javax.servlet.http.HttpServletResponse;
 import javax.websocket.Session;
 
 public class GetGraphMessageDispatcher extends MessageDispatcher {
+
     private final Controller controller;
     private final Session session;
     private final MessageType type;
 
-    private GraphSettings settings;
+    private GraphSettings graphSettings;
     private DirectedWeightedGraph graph;
 
     public GetGraphMessageDispatcher(Controller controller, Session session, MessageType type) {
-	super(controller, session, type);
-	this.controller = controller;
-	this.session = session;
-	this.type = type;
+        super(controller, session, type);
+        this.controller = controller;
+        this.session = session;
+        this.type = type;
     }
 
     @Override
     boolean setRequest(MessageRequest request) throws Exception {
-	this.request = request;
-	this.settings = new GraphSettings(this.request.getData());
-	
-	return true;
+        this.request = request;
+        this.graphSettings = new GraphSettings(this.request.getData());
+
+        return true;
     }
 
     @Override
     void setParameters(ArrayList<Object> parameters) {
-	this.parameters = parameters;
+        this.parameters = parameters;
     }
 
     @Override
     boolean process() {
-	MessageResponse response = new MessageResponse(this.request.getCallbackId());
+        MessageResponse response = new MessageResponse(this.request.getCallbackId());
 
-	try {
-	    this.graph = new DirectedWeightedGraph(this.settings);
+        try {
+            this.graph = new DirectedWeightedGraph(this.graphSettings);
 
-	    this.graph.initNodes();
-	    this.graph.initEdges();
+            this.graph.initNodes();
+            this.graph.initEdges();
 
-	    response
-		    .setStatus(HttpServletResponse.SC_OK)
-		    .setIsEnded(true)
-		    .setDescription("Ok")
-		    .setData(graph.toJson());
-	    updateProgress(response);
+            response
+                    .setStatus(HttpServletResponse.SC_OK)
+                    .setIsEnded(true)
+                    .setDescription("Ok")
+                    .setData(graph.toJson());
+            updateProgress(response);
 
-	} catch (Exception e) {
-	    response.setStatus(HttpServletResponse.SC_BAD_REQUEST).setDescription(e.getMessage());
-	    updateProgress(response);
+        } catch (Exception e) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST).setDescription(e.getMessage());
+            updateProgress(response);
 
-	    Logger.getLogger(Controller.class.getName()).log(Level.SEVERE,
-		    "There was an error generating the graph: " + e.getMessage(), e);
-	    return false;
-	}
+            Logger.getLogger(Controller.class.getName()).log(Level.SEVERE,
+                    "There was an error generating the graph: " + e.getMessage(), e);
+            return false;
+        }
 
-	return true;
+        return true;
     }
 
     @Override
     protected void updateProgress(MessageResponse response) {
-	this.controller.respond(this.session, response);
+        this.controller.respond(this.session, response);
     }
 
     public DirectedWeightedGraph getGraph() {
-	return graph;
+        return this.graph;
+    }
+
+    public GraphSettings getGraphSettings() {
+        return this.graphSettings;
     }
 }
