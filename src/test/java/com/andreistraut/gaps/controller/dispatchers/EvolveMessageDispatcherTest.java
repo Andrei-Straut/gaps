f69f582fd9b568597d44a93ab38856d04f179f0b
Andrei Straut <andrei.straut@gmail.com>
2015-04-22 17:33:36 +0100

    EvolveMessageDispatcher unit test stub

---
+++ src/test/java/com/andreistraut/gaps/controller/dispatchers/EvolveMessageDispatcherTest.java
@@ -0,0 +1,48 @@
+
+package com.andreistraut.gaps.controller.dispatchers;
+
+import com.andreistraut.gaps.controller.MessageRequest;
+import com.andreistraut.gaps.controller.MessageResponse;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class EvolveMessageDispatcherTest {
+    
+    public EvolveMessageDispatcherTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+        Logger.getLogger(EvolveMessageDispatcherTest.class.getName()).log(Level.INFO,
+                "{0} TEST: EvolveMessageDispatcher",
+                EvolveMessageDispatcherTest.class.toString());
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @Test
+    public void testSetRequest() throws Exception {
+    }
+
+    @Test
+    public void testSetParameters() throws Exception {
+    }
+
+    @Test
+    public void testProcess() throws Exception {
+    }
+
+    @Test
+    public void testUpdateProgress() {
+    }
+    
+}
