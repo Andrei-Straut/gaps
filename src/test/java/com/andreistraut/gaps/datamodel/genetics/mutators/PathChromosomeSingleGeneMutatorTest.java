4ad4ccd1601a3edd451d3588917056f009f3aea3
Andrei Straut <andrei.straut@gmail.com>
2015-05-04 18:00:39 +0100

    Add mutation probability = 100 percent and 0 percent tests to SingleGeneMutator (still WIP)

--- src/test/java/com/andreistraut/gaps/datamodel/genetics/mutators/PathChromosomeSingleGeneMutatorTest.java
+++ src/test/java/com/andreistraut/gaps/datamodel/genetics/mutators/PathChromosomeSingleGeneMutatorTest.java
@@ -11,7 +11,9 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.jgap.InvalidConfigurationException;
 import org.jgap.RandomGenerator;
+import org.jgap.impl.StockRandomGenerator;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -23,6 +25,7 @@ public class PathChromosomeSingleGeneMutatorTest {
     private GeneticConfiguration conf;
     private ArrayList<EdgeGene> genes;
     private PathChromosome chromosome;
+    private PathChromosome expected;
     private final int RUN_LIMIT = 10;
     
     public PathChromosomeSingleGeneMutatorTest() {
@@ -40,25 +43,59 @@ public class PathChromosomeSingleGeneMutatorTest {
 	this.graphMock = new DirectedWeightedGraphMockThreeNodeThreeEdges();
 	this.conf = new GeneticConfiguration("DirectedWeightedGraphMockThreeNodeThreeEdges", this.graphMock.getGraph());
 	this.conf.setKeepPopulationSizeConstant(false);
-	
-	EdgeGene firstToThirdGene = new EdgeGene(graphMock.getFirstToThirdEdge(), conf);
-	this.genes = new ArrayList<EdgeGene>();
-	this.genes.add(firstToThirdGene);
-	this.chromosome = new PathChromosome(conf, genes, graphMock.getFirstNode(), graphMock.getThirdNode());
 
-	DirectedWeightedEdge firstToThird = new DirectedWeightedEdge(
+	DirectedWeightedEdge firstToThirdMoreExpensive = new DirectedWeightedEdge(
 		graphMock.getFirstNode(), 
 		graphMock.getThirdNode(), 
-		graphMock.getFirstToThirdEdge().getCost() + 1);	
-	graphMock.getGraph().addEdge(graphMock.getFirstNode(), graphMock.getThirdNode(), firstToThird);
+		graphMock.getFirstToThirdEdge().getCost() + 1);
+	graphMock.getGraph().addEdge(graphMock.getFirstNode(), graphMock.getThirdNode(), firstToThirdMoreExpensive);
+	
+	EdgeGene firstToThirdGeneMoreExpensive = new EdgeGene(firstToThirdMoreExpensive, conf);
+	this.genes = new ArrayList<EdgeGene>();
+	this.genes.add(firstToThirdGeneMoreExpensive);
+	this.chromosome = new PathChromosome(conf, genes, graphMock.getFirstNode(), graphMock.getThirdNode());
+
+	ArrayList<EdgeGene> expectedGenes = new ArrayList<EdgeGene>();
+	expectedGenes.add(new EdgeGene(graphMock.getFirstToThirdEdge(), conf));
+	expected = new PathChromosome(conf, expectedGenes,
+		graphMock.getFirstNode(), graphMock.getThirdNode());
     }
 
     @Test
-    public void testDoMutation() throws Exception {
+    public void testDoMutation100PercentBetterSection() throws Exception {
+	for (int i = 0; i < this.RUN_LIMIT; i++) {
+	    this.setUp();
+	    
+	    ArrayList<PathChromosome> candidates = new ArrayList<PathChromosome>();
+	    candidates.add(this.chromosome);
+
+	    PathChromosomeSingleGeneMutator mutator = new PathChromosomeSingleGeneMutator(conf, 100);
+	    Assert.assertFalse(this.chromosome.equals(expected));
+	    
+	    mutator.doMutation(this.chromosome, candidates, new StockRandomGenerator());
+
+	    Assert.assertTrue(candidates.size() == 1);
+	    Assert.assertTrue(this.chromosome.equals(expected));
+	    Assert.assertTrue(candidates.contains(this.chromosome));
+	}
     }
 
     @Test
-    public void testGetMutationPosition() {
-    }
-    
+    public void testDoMutation0PercentBetterSection() throws Exception {
+	for (int i = 0; i < this.RUN_LIMIT; i++) {
+	    this.setUp();
+	    
+	    ArrayList<PathChromosome> candidates = new ArrayList<PathChromosome>();
+	    candidates.add(this.chromosome);
+
+	    PathChromosomeSingleGeneMutator mutator = new PathChromosomeSingleGeneMutator(conf, 0);
+	    Assert.assertFalse(this.chromosome.equals(expected));
+	    
+	    mutator.doMutation(this.chromosome, candidates, new StockRandomGenerator());
+
+	    Assert.assertTrue(candidates.size() == 1);
+	    Assert.assertFalse(this.chromosome.equals(expected));
+	    Assert.assertTrue(candidates.contains(this.chromosome));
+	}
+    }    
 }
