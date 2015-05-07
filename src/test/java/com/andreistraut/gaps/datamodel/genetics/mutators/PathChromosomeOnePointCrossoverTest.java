cc41120b54014a219b869046e293306b39636703
Andrei Straut <andrei.straut@gmail.com>
2015-05-07 09:14:45 +0100

    Tests for OnePointCrossover

--- src/test/java/com/andreistraut/gaps/datamodel/genetics/mutators/PathChromosomeOnePointCrossoverTest.java
+++ src/test/java/com/andreistraut/gaps/datamodel/genetics/mutators/PathChromosomeOnePointCrossoverTest.java
@@ -3,6 +3,7 @@ package com.andreistraut.gaps.datamodel.genetics.mutators;
 import com.andreistraut.gaps.datamodel.genetics.EdgeGene;
 import com.andreistraut.gaps.datamodel.genetics.GeneticConfiguration;
 import com.andreistraut.gaps.datamodel.genetics.PathChromosome;
+import com.andreistraut.gaps.datamodel.genetics.PathChromosomePopulation;
 import com.andreistraut.gaps.datamodel.graph.DirectedWeightedEdge;
 import com.andreistraut.gaps.datamodel.mock.DirectedWeightedGraphMockThreeNodeThreeEdges;
 import com.andreistraut.gaps.datamodel.mock.PathChromosomeMockOneGene;
@@ -112,7 +113,6 @@ public class PathChromosomeOnePointCrossoverTest {
 	    candidates.add(this.secondChromosome);
 
 	    PathChromosomeOnePointCrossover mutator = new PathChromosomeOnePointCrossover(conf, 0);
-	    mutator.setPrintCrossoverStatistics(true);
 	    Assert.assertFalse(this.firstChromosome.equals(this.secondChromosome));
 	    Assert.assertTrue(this.firstChromosome.isLegal());
 	    Assert.assertTrue(this.secondChromosome.isLegal());
@@ -140,6 +140,54 @@ public class PathChromosomeOnePointCrossoverTest {
 		}
 	    }
 	    Assert.assertTrue(containsGene);
+	}
+    }    
+
+    @Test
+    public void testOperateTwoCandidatesValid100PercentKeepPopulationSizeConstant() throws Exception {
+	for (int i = 0; i < this.RUN_LIMIT; i++) {
+	    this.setUp();
+	    
+	    ArrayList<PathChromosome> candidates = new ArrayList<PathChromosome>();
+	    candidates.add(this.firstChromosome);
+	    candidates.add(this.secondChromosome);
+
+	    PathChromosomeOnePointCrossover mutator = new PathChromosomeOnePointCrossover(conf, 100);
+	    conf.setKeepPopulationSizeConstant(true);
+	    Assert.assertFalse(this.firstChromosome.equals(this.secondChromosome));
+	    Assert.assertTrue(this.firstChromosome.isLegal());
+	    Assert.assertTrue(this.secondChromosome.isLegal());
+
+	    PathChromosomePopulation population = new PathChromosomePopulation(conf, candidates);
+	    mutator.operate(population, candidates);
+	    
+	    Assert.assertTrue(this.firstChromosome.isLegal());
+	    Assert.assertTrue(this.secondChromosome.isLegal());
+	    Assert.assertFalse(this.firstChromosome.equals(secondChromosome));
+	}
+    }
+
+    @Test
+    public void testOperateTwoCandidatesValid00PercentKeepPopulationSizeConstant() throws Exception {
+	for (int i = 0; i < this.RUN_LIMIT; i++) {
+	    this.setUp();
+	    
+	    ArrayList<PathChromosome> candidates = new ArrayList<PathChromosome>();
+	    candidates.add(this.firstChromosome);
+	    candidates.add(this.secondChromosome);
+
+	    PathChromosomeOnePointCrossover mutator = new PathChromosomeOnePointCrossover(conf, 0);
+	    conf.setKeepPopulationSizeConstant(true);
+	    Assert.assertFalse(this.firstChromosome.equals(this.secondChromosome));
+	    Assert.assertTrue(this.firstChromosome.isLegal());
+	    Assert.assertTrue(this.secondChromosome.isLegal());
+
+	    PathChromosomePopulation population = new PathChromosomePopulation(conf, candidates);
+	    mutator.operate(population, candidates);
+	    
+	    Assert.assertTrue(this.firstChromosome.isLegal());
+	    Assert.assertTrue(this.secondChromosome.isLegal());
+	    Assert.assertFalse(this.firstChromosome.equals(secondChromosome));
 	}
     }
 }
