1455e38a32f06daa5e9afeba804a793e04efd39b
Andrei Straut <andrei.straut@gmail.com>
2015-05-08 15:24:07 +0100

    GraphSettings tests

--- src/main/java/com/andreistraut/gaps/datamodel/graph/GraphSettings.java
+++ src/main/java/com/andreistraut/gaps/datamodel/graph/GraphSettings.java
@@ -1,15 +1,15 @@
-
 package com.andreistraut.gaps.datamodel.graph;
 
 import com.google.gson.JsonObject;
 
 public class GraphSettings {
+
     private int numberOfNodes;
     private int numberOfEdges;
     private int minimumEdgeWeight;
     private int maximumEdgeWeight;
     private boolean isStatic;
-    
+
     public GraphSettings(int numberOfNodes, int numberOfEdges) {
 	this.numberOfNodes = numberOfNodes;
 	this.numberOfEdges = numberOfEdges;
@@ -17,7 +17,7 @@ public class GraphSettings {
 	this.maximumEdgeWeight = 1;
 	this.isStatic = true;
     }
-    
+
     public GraphSettings(JsonObject settings) throws Exception {
 	this.fromJson(settings);
     }
@@ -61,39 +61,39 @@ public class GraphSettings {
     public void setIsStatic(boolean isStatic) {
 	this.isStatic = isStatic;
     }
-    
+
     public JsonObject toJson() {
 	JsonObject settings = new JsonObject();
-	
+
 	settings.addProperty("numberOfNodes", this.numberOfNodes);
 	settings.addProperty("numberOfEdges", this.numberOfEdges);
 	settings.addProperty("minimumEdgeWeight", this.minimumEdgeWeight);
 	settings.addProperty("maximumEdgeWeight", this.maximumEdgeWeight);
 	settings.addProperty("isStatic", this.isStatic);
-	
+
 	return settings;
     }
-    
+
     public GraphSettings fromJson(JsonObject settings) throws Exception {
-	if(!settings.has("numberOfNodes") || !settings.has("numberOfEdges")) {
+	if (!settings.has("numberOfNodes") || !settings.has("numberOfEdges")) {
 	    throw new Exception("Number of nodes and number of edges must always be specified!");
 	}
-	
+
 	this.numberOfNodes = settings.get("numberOfNodes").getAsInt();
 	this.numberOfEdges = settings.get("numberOfEdges").getAsInt();
+
+	this.minimumEdgeWeight = settings.has("minimumEdgeWeight")
+		? settings.get("minimumEdgeWeight").getAsInt()
+		: 1;
 	
-	if(settings.has("minimumEdgeWeight")) {
-	    this.minimumEdgeWeight = settings.get("minimumEdgeWeight").getAsInt();
-	}
-	
-	if(settings.has("maximumEdgeWeight")) {
-	    this.maximumEdgeWeight = settings.get("maximumEdgeWeight").getAsInt();
-	}
-	
-	if(settings.has("isStatic")) {
-	    this.isStatic = settings.get("isStatic").getAsBoolean();
-	}
-	
+	this.maximumEdgeWeight = settings.has("maximumEdgeWeight") 
+		? settings.get("maximumEdgeWeight").getAsInt() 
+		: 1;
+
+	this.isStatic = settings.has("isStatic") 
+		? settings.get("isStatic").getAsBoolean()
+		: true;
+
 	return this;
     }
 }
--- src/test/java/com/andreistraut/gaps/datamodel/graph/GraphSettingsTest.java
+++ src/test/java/com/andreistraut/gaps/datamodel/graph/GraphSettingsTest.java
@@ -3,34 +3,186 @@ package com.andreistraut.gaps.datamodel.graph;
 import com.google.gson.JsonObject;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
 import org.junit.BeforeClass;
 
 public class GraphSettingsTest {
-    
+
     public GraphSettingsTest() {
     }
 
     @BeforeClass
     public static void setUpClass() {
-	Logger.getLogger(GraphSettingsTest.class.getName()).log(Level.INFO, 
-		"{0} TEST: GraphSettings", 
+	Logger.getLogger(GraphSettingsTest.class.getName()).log(Level.INFO,
+		"{0} TEST: GraphSettings",
 		GraphSettingsTest.class.toString());
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @Test
-    public void testToJson() {
+    public void testConstructorIntValues() {
+	GraphSettings settings = new GraphSettings(10, 10);
+
+	Assert.assertTrue(settings.getNumberOfNodes() == 10);
+	Assert.assertTrue(settings.getNumberOfEdges() == 10);
+	Assert.assertTrue(settings.getMinimumEdgeWeight() == 1);
+	Assert.assertTrue(settings.getMaximumEdgeWeight() == 1);
+	Assert.assertTrue(settings.isStatic());
     }
 
     @Test
-    public void testFromJson() throws Exception {
+    public void testConstructorJsonObjectValid() throws Exception {
+	JsonObject settingsJson = new JsonObject();
+	settingsJson.addProperty("numberOfNodes", 15);
+	settingsJson.addProperty("numberOfEdges", 25);
+	settingsJson.addProperty("minimumEdgeWeight", 10);
+	settingsJson.addProperty("maximumEdgeWeight", 100);
+	settingsJson.addProperty("isStatic", true);
+
+	GraphSettings settings = new GraphSettings(settingsJson);
+
+	Assert.assertTrue(settings.getNumberOfNodes() == 15);
+	Assert.assertTrue(settings.getNumberOfEdges() == 25);
+	Assert.assertTrue(settings.getMinimumEdgeWeight() == 10);
+	Assert.assertTrue(settings.getMaximumEdgeWeight() == 100);
+	Assert.assertTrue(settings.isStatic());
     }
-    
+
+    @Test
+    public void testConstructorJsonObjectDefaultValueMinEdgeWeight() throws Exception {
+	JsonObject settingsJson = new JsonObject();
+	settingsJson.addProperty("numberOfNodes", 15);
+	settingsJson.addProperty("numberOfEdges", 25);
+	settingsJson.addProperty("maximumEdgeWeight", 100);
+	settingsJson.addProperty("isStatic", true);
+
+	GraphSettings settings = new GraphSettings(settingsJson);
+	Assert.assertTrue(settings.getMinimumEdgeWeight() == 1);
+    }
+
+    @Test
+    public void testConstructorJsonObjectDefaultValueMaxEdgeWeight() throws Exception {
+	JsonObject settingsJson = new JsonObject();
+	settingsJson.addProperty("numberOfNodes", 15);
+	settingsJson.addProperty("numberOfEdges", 25);
+	settingsJson.addProperty("minimumEdgeWeight", 100);
+	settingsJson.addProperty("isStatic", true);
+
+	GraphSettings settings = new GraphSettings(settingsJson);
+	Assert.assertTrue(settings.getMaximumEdgeWeight() == 1);
+    }
+
+    @Test
+    public void testConstructorJsonObjectDefaultValueIsStatic() throws Exception {
+	JsonObject settingsJson = new JsonObject();
+	settingsJson.addProperty("numberOfNodes", 15);
+	settingsJson.addProperty("numberOfEdges", 25);
+	settingsJson.addProperty("minimumEdgeWeight", 100);
+	settingsJson.addProperty("maximumEdgeWeight", 100);
+
+	GraphSettings settings = new GraphSettings(settingsJson);
+	Assert.assertTrue(settings.isStatic());
+    }
+
+    @Test
+    public void testConstructorJsonObjectDefaultValues() throws Exception {
+	JsonObject settingsJson = new JsonObject();
+	settingsJson.addProperty("numberOfNodes", 15);
+	settingsJson.addProperty("numberOfEdges", 25);
+
+	GraphSettings settings = new GraphSettings(settingsJson);
+	Assert.assertTrue(settings.getMinimumEdgeWeight() == 1);
+	Assert.assertTrue(settings.getMaximumEdgeWeight() == 1);
+	Assert.assertTrue(settings.isStatic());
+    }
+
+    @Test
+    public void testConstructorJsonObjectInvalidMissingNumberOfNodes() {
+	JsonObject settingsJson = new JsonObject();
+	settingsJson.addProperty("numberOfEdges", 25);
+	settingsJson.addProperty("minimumEdgeWeight", 10);
+	settingsJson.addProperty("maximumEdgeWeight", 100);
+	settingsJson.addProperty("isStatic", true);
+
+	try {
+	    GraphSettings settings = new GraphSettings(settingsJson);
+	} catch (Exception e) {
+	    Assert.assertTrue(e.getMessage().contains("Number of nodes and number of edges must always be specified"));
+	}
+    }
+
+    @Test
+    public void testConstructorJsonObjectInvalidMissingNumberOfEdges() {
+	JsonObject settingsJson = new JsonObject();
+	settingsJson.addProperty("numberOfNodes", 25);
+	settingsJson.addProperty("minimumEdgeWeight", 10);
+	settingsJson.addProperty("maximumEdgeWeight", 100);
+	settingsJson.addProperty("isStatic", true);
+
+	try {
+	    GraphSettings settings = new GraphSettings(settingsJson);
+	} catch (Exception e) {
+	    Assert.assertTrue(e.getMessage().contains("Number of nodes and number of edges must always be specified"));
+	}
+    }
+
+    @Test
+    public void testConstructorJsonObjectInvalidMissingNumberOfNodesNumberOfEdges() {
+	JsonObject settingsJson = new JsonObject();
+	settingsJson.addProperty("minimumEdgeWeight", 10);
+	settingsJson.addProperty("maximumEdgeWeight", 100);
+	settingsJson.addProperty("isStatic", true);
+
+	try {
+	    GraphSettings settings = new GraphSettings(settingsJson);
+	} catch (Exception e) {
+	    Assert.assertTrue(e.getMessage().contains("Number of nodes and number of edges must always be specified"));
+	}
+    }
+
+    @Test
+    public void testToJsonWithConstructorIntegerValues() {
+	GraphSettings settings = new GraphSettings(10, 10);
+	
+	JsonObject settingsJson = settings.toJson();
+	Assert.assertTrue(settingsJson.get("numberOfNodes").getAsInt() == 10);
+	Assert.assertTrue(settingsJson.get("numberOfEdges").getAsInt() == 10);
+	Assert.assertTrue(settingsJson.get("minimumEdgeWeight").getAsInt() == 1);
+	Assert.assertTrue(settingsJson.get("maximumEdgeWeight").getAsInt() == 1);
+	Assert.assertTrue(settingsJson.get("isStatic").getAsBoolean());
+    }
+
+    @Test
+    public void testToJsonWithConstructorDefaultIntegerValues() {
+	GraphSettings settings = new GraphSettings(10, 10);
+	settings.setMinimumEdgeWeight(100);
+	settings.setMaximumEdgeWeight(150);
+	settings.setIsStatic(false);
+	
+	JsonObject settingsJson = settings.toJson();
+	Assert.assertTrue(settingsJson.get("numberOfNodes").getAsInt() == 10);
+	Assert.assertTrue(settingsJson.get("numberOfEdges").getAsInt() == 10);
+	Assert.assertTrue(settingsJson.get("minimumEdgeWeight").getAsInt() == 100);
+	Assert.assertTrue(settingsJson.get("maximumEdgeWeight").getAsInt() == 150);
+	Assert.assertFalse(settingsJson.get("isStatic").getAsBoolean());
+    }
+
+    @Test
+    public void testToJsonWithConstructorJsonObject() throws Exception {
+	JsonObject settingsJsonInit = new JsonObject();
+	settingsJsonInit.addProperty("numberOfNodes", 15);
+	settingsJsonInit.addProperty("numberOfEdges", 25);
+	settingsJsonInit.addProperty("minimumEdgeWeight", 10);
+	settingsJsonInit.addProperty("maximumEdgeWeight", 100);
+	settingsJsonInit.addProperty("isStatic", true);
+	GraphSettings settings = new GraphSettings(settingsJsonInit);
+	
+	JsonObject settingsJsonToJson = settings.toJson();
+	Assert.assertTrue(settingsJsonInit.equals(settingsJsonToJson));
+    }
 }
