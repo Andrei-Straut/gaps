15cca86a922a7f06952f73c5c181b249bbfb5a5f
Andrei Straut <andrei.straut@gmail.com>
2017-08-23 10:52:36 +0100

    Revert

--- src/main/webapp/scripts/gaps/app.gaps.js
+++ src/main/webapp/scripts/gaps/app.gaps.js
@@ -1,4 +1,4 @@
-var wsURL = "wss://localhost:53438/gaps/controller";
+var wsURL = "wss://andreistraut.info:8443/gaps/controller";
 
 /* global angular */
 var gaps = angular.module('gaps', ['ui.bootstrap', 'ui-notification']);
